0940b07034c5ff3efd047ba8861787e64ea2ffef
Enrique Gil <enriquegq1093@hotmail.com>
2025-02-11 10:59:44 -0500

    feature(experience): update of experience order

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
 import Timeline from '@mui/lab/Timeline';
-import TimelineItem,  { timelineItemClasses }  from '@mui/lab/TimelineItem';
+import TimelineItem, { timelineItemClasses } from '@mui/lab/TimelineItem';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
@@ -17,11 +17,12 @@ function Experience() {
     const [loading, setLoading] = useState(false);
     const [experience, setExperience] = useState([])
 
+    console.log('11', experience)
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
     const host = import.meta.env.VITE_HOST;
- 
+
     async function fetchSearchAllExperience() {
         setLoading(true);
         try {
@@ -44,6 +45,14 @@ function Experience() {
         }
     }
 
+    const sortByConditions = (arr) =>
+        arr.sort((a, b) => {
+            if (a.endDate === null && b.endDate !== null) return -1;
+            if (a.endDate !== null && b.endDate === null) return 1;
+            return new Date(b.startDate) - new Date(a.startDate);
+        });
+
+    const dataOrdered = sortByConditions(experience);
 
     useEffect(() => {
         fetchSearchAllExperience()
@@ -53,86 +62,86 @@ function Experience() {
         <div className="flex justify-start items-start">
             <Logo open={loading} />
             {experience.length === 0 ? (
-    <Typography> Sin Resultados</Typography>
-) : (
-    <Timeline sx={
-        isMobile
-          ? {
-              [`& .${timelineItemClasses.root}:before`]: {
-                flex: 0,
-                padding: 0,
-              },
-            }
-          : {}
-      }
-      position={isMobile ? "left" : "alternate"}>
-        {experience.map((item, index) => (
-            <TimelineItem
-                key={item._id}
-                className="hover:scale-110 active:scale-110 focus-within:scale-110 transition-transform duration-300"
-            >
-                <TimelineSeparator>
-                    <TimelineDot />
-                    {index < experience.length - 1 && <TimelineConnector />}
-                </TimelineSeparator>
-                <TimelineContent className="relative">
-                    <Typography
-                        variant="h6"
-                        sx={{
-                            fontSize: { xs: '1.2rem', sm: '1.5rem' },
-                            fontWeight: 'bold',
-                            zIndex: 10,  
-                        }}
-                    >
-                        {item.company}
-                    </Typography>
-                    <Typography
-                        variant="body1"
-                        sx={{
-                            fontSize: { xs: '0.9rem', sm: '1rem' },
-                            fontWeight: 'bold',
-                        }}
-                    >
-                        {item.position} - {item.typePosition}
-                    </Typography>
-                    <Typography
-                        variant="body2"
-                        sx={{
-                            fontSize: { xs: '0.8rem', sm: '1rem' },
-                            color: 'text.secondary',
-                        }}
-                    >
-                        {format(new Date(item.startDate), 'MMM yyyy')}
-                        - {item.endDate ? format(new Date(item.endDate), 'MMM, yyyy') : 'Presente'}
-                    </Typography>
-                    <Typography
-                        variant="body2"
-                        sx={{
-                            fontSize: { xs: '0.8rem', sm: '1rem' },
-                            color: 'text.secondary',
-                            marginTop: '0.5rem',
-                        }}
-                    >
-                        <ul>
-                            {item?.description?.map((desc, index) => (
-                                <li key={index}>{desc}</li>
-                            ))}
-                        </ul>
-                    </Typography>
-                    <Typography
-                        variant="body2"
-                        sx={{
-                            fontSize: { xs: '0.8rem', sm: '1rem' },
-                            color: 'text.secondary',
-                        }}
-                    >
-                        <strong> Ubicación:</strong> {item.location}
-                    </Typography>
-                </TimelineContent>
-            </TimelineItem>
-        ))}
-    </Timeline>
-)}
+                <Typography> Sin Resultados</Typography>
+            ) : (
+                <Timeline sx={
+                    isMobile
+                        ? {
+                            [`& .${timelineItemClasses.root}:before`]: {
+                                flex: 0,
+                                padding: 0,
+                            },
+                        }
+                        : {}
+                }
+                    position={isMobile ? "left" : "alternate"}>
+                    {dataOrdered.map((item, index) => (
+                        <TimelineItem
+                            key={item._id}
+                            className="hover:scale-110 active:scale-110 focus-within:scale-110 transition-transform duration-300"
+                        >
+                            <TimelineSeparator>
+                                <TimelineDot />
+                                {index < dataOrdered.length - 1 && <TimelineConnector />}
+                            </TimelineSeparator>
+                            <TimelineContent className="relative">
+                                <Typography
+                                    variant="h6"
+                                    sx={{
+                                        fontSize: { xs: '1.2rem', sm: '1.5rem' },
+                                        fontWeight: 'bold',
+                                        zIndex: 10,
+                                    }}
+                                >
+                                    {item.company}
+                                </Typography>
+                                <Typography
+                                    variant="body1"
+                                    sx={{
+                                        fontSize: { xs: '0.9rem', sm: '1rem' },
+                                        fontWeight: 'bold',
+                                    }}
+                                >
+                                    {item.position} - {item.typePosition}
+                                </Typography>
+                                <Typography
+                                    variant="body2"
+                                    sx={{
+                                        fontSize: { xs: '0.8rem', sm: '1rem' },
+                                        color: 'text.secondary',
+                                    }}
+                                >
+                                    {format(new Date(item.startDate), 'MMM yyyy')}
+                                    - {item.endDate ? format(new Date(item.endDate), 'MMM, yyyy') : 'Presente'}
+                                </Typography>
+                                <Typography
+                                    variant="body2"
+                                    sx={{
+                                        fontSize: { xs: '0.8rem', sm: '1rem' },
+                                        color: 'text.secondary',
+                                        marginTop: '0.5rem',
+                                    }}
+                                >
+                                    <ul>
+                                        {item?.description?.map((desc, index) => (
+                                            <li key={index}>{desc}</li>
+                                        ))}
+                                    </ul>
+                                </Typography>
+                                <Typography
+                                    variant="body2"
+                                    sx={{
+                                        fontSize: { xs: '0.8rem', sm: '1rem' },
+                                        color: 'text.secondary',
+                                    }}
+                                >
+                                    <strong> Ubicación:</strong> {item.location}
+                                </Typography>
+                            </TimelineContent>
+                        </TimelineItem>
+                    ))}
+                </Timeline>
+            )}
 
         </div>
     )
